4e9a994fbb65b81fa1d53c85765752ba9bb65d5a
Alexander Permyakov <pedectrian@ruwizards.com>
2014-02-09 18:13:58 +0400

    part5, testing app

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,50 @@
-from django.http import HttpResponse
-from polls.models import Poll
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+from polls.models import Poll, Choice
 
-def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
 
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    def get_queryset(self):
+        """Return the last five polls (not including those set to be
+        published in the future).
+        """
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any polls that aren't published yet
+        """
+        return Poll.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+   model = Poll
+   template_name = 'polls/results.html'
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message' : "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
